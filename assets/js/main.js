c7f18965281f0537d3be9a5fcf87f006ec08e725
wynnytran <wynny.me@gmail.com>
2020-10-16 15:55:04 +0300

    feat: add smooth scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,22 +5,36 @@ navToggle.addEventListener('click', () => {
 	navMenu.classList.toggle('show');
 });
 
-const navLinks = document.querySelectorAll('.nav__link');
-
-navLinks.forEach((link) => {
-	link.addEventListener('click', (e) => {
-		// e.preventDefault();
-		navMenu.classList.remove('show');
-	});
-});
-
-// Change Nav style On scroll
 const header = document.getElementById('header');
 const home = document.getElementById('home');
 const sectionOneOptions = {
 	rootMargin: '-650px 0px 0px 0px',
 };
 
+// Smooth scroll
+const navLinks = document.querySelectorAll('.nav__link');
+navLinks.forEach((link) => {
+	link.addEventListener('click', (e) => {
+		e.preventDefault();
+		const id = e.currentTarget.getAttribute('href').slice(1);
+		const element = document.getElementById(id);
+		const navHeight = header.getBoundingClientRect().height;
+		const fixedNav = header.classList.contains('fixed');
+		let position = element.offsetTop - navHeight;
+		if (!fixedNav) {
+			position = position - navHeight;
+		}
+		window.scrollTo({
+			left: 0,
+			top: position,
+		});
+
+		console.log(position);
+		navMenu.classList.remove('show');
+	});
+});
+
+// Change Nav style On scroll
 const sectionObserverOne = new IntersectionObserver(
 	(entries, sectionObserverOne) => {
 		entries.forEach((entry) => {
@@ -35,3 +49,11 @@ const sectionObserverOne = new IntersectionObserver(
 );
 
 sectionObserverOne.observe(home);
+
+// Smooth scroll
+
+// const scrollLinks = document.querySelectorAll('.scroll__link');
+
+// scrollLinks.forEach((link)=>{
+// 	link.addEventListener
+// })
