5feb5bc3d8730b7e104abff2e18f5e43cc461f9e
wynnytran <wynny.me@gmail.com>
2020-10-16 16:13:52 +0300

    feat: add scroll reveal

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,10 +50,27 @@ const sectionObserverOne = new IntersectionObserver(
 
 sectionObserverOne.observe(home);
 
-// Smooth scroll
+const sr = ScrollReveal({
+	origin: 'top',
+	distance: '80px',
+	duration: 2000,
+	reset: true,
+});
 
-// const scrollLinks = document.querySelectorAll('.scroll__link');
+/*SCROLL HOME*/
+sr.reveal('.home__title', {});
+sr.reveal('.button', { delay: 200 });
+sr.reveal('.home__img', { delay: 400 });
+sr.reveal('.home__social-icon', { interval: 200 });
 
-// scrollLinks.forEach((link)=>{
-// 	link.addEventListener
-// })
+/*SCROLL SKILLS*/
+sr.reveal('.skills__subtitle', {});
+sr.reveal('.skills__text', {});
+sr.reveal('.skills__data', { interval: 200 });
+sr.reveal('.skills__img', { delay: 600 });
+
+/*SCROLL WORK*/
+sr.reveal('.work__img', { interval: 200 });
+
+/*SCROLL CONTACT*/
+sr.reveal('.contact__input', { interval: 200 });
